f464cd94ad05691b1f5d44db6ca3ec036ef2a54a
Praveen-9640 <rapetipraveen5@gmail.com>
2026-04-28 00:47:19 +0530

    connect frontend to deployed backend

--- src/api.js
+++ src/api.js
@@ -1,20 +1,25 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api', // ✅ use proxy
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default api;
